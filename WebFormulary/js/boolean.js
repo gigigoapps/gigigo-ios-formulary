c5a475930534f940b00e24789b8d4bc3e7321c41
Eduardo Parada <udelunar@gmail.com>
2016-07-29 10:32:26 +0200

    boolean and datepicker web

--- WebFormulary/js/boolean.js
+++ WebFormulary/js/boolean.js
@@ -2,7 +2,7 @@
 //               BOOLEAN              //
 //======================================
             
-function createBooleanField(keyTextField,title,error,mandatory,cellColor,titleColor,errorColor,sizeTitle,sizeError) {    
+function createBooleanField(keyTextField,title,error,mandatory,cellColor,titleColor,errorColor,sizeTitle,sizeError,align,font) {    
     var valueCheck = ""
     if (mandatory) {
         valueCheck = "checked"
@@ -11,7 +11,8 @@ function createBooleanField(keyTextField,title,error,mandatory,cellColor,titleCo
     //-- Recover Styles --
     var htmlBackgroundColor = getStyleColor(cellColor,titleColor,errorColor);
     var htmlFontSize = getStyleSize (sizeTitle, sizeError);
-    var styles =  htmlFontSize + htmlBackgroundColor;
+    var htmlAlingFont = getAlignFont(align,font)
+    var styles =  htmlFontSize + htmlBackgroundColor + htmlAlingFont;
 
     var html = '<div class="cellConstructor"id="fieldNumber'+indexField+'"><div class="row"><div class="col-md-10"><div class="keyTextField"><p>key*:</p><input type="text"name="keyTextField"id="keyTextField"disabled value="'+keyTextField+'"></div><div class="containerTextFieldTop"><div class="titleTextField"><p>Titulo*:</p><input type="text"name="titleTextField"id="titleTextField"disabled value="'+title+'"></div></div><div class="containerTextFieldCenter"><div class="mandatoryTextField"><input type="checkbox"name="mandatory"value="mandatory"id="mandatory"'+valueCheck+'disabled readonly><p>Es obligatorio?</p></div></div><div class="errorTextField"><p class="textErrorP">Texto error:</p><input type="text"name="errorTextField"id="errorTextField"disabled value="'+error+'"></div><div class="styleField"><h4>Estilos de celda:</h4>'+styles+'</div><div class="spaceSeparate"></div></div><div class="col-md-2 buttonRemove buttonRemoveText"onclick="removeField('+indexField+')"><p>-</p></div></div></div>';
 
@@ -19,7 +20,7 @@ function createBooleanField(keyTextField,title,error,mandatory,cellColor,titleCo
     resetTypeField();
 }
 
-function saveBooleanField(keyTextField,type,title,error,mandatory,cellColor,titleColor,errorColor,sizeTitle,sizeError) {
+function saveBooleanField(keyTextField,type,title,error,mandatory,cellColor,titleColor,errorColor,sizeTitle,sizeError,align,font) {
     //-- Mandatory Fiedls --
     var itemSave = {
         "key":keyTextField,
@@ -35,7 +36,7 @@ function saveBooleanField(keyTextField,type,title,error,mandatory,cellColor,titl
     }  
     
     //-- OPTIONAL FIELDS --
-    var styles = getStylesJson(cellColor,titleColor,errorColor,sizeTitle,sizeError,"","","");
+    var styles = getStylesJson(cellColor,titleColor,errorColor,sizeTitle,sizeError,"","","",align,font);
 
     if (styles != null) {
         itemSave["style"] = styles
